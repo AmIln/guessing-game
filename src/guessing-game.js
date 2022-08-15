ab0337a95eb18a3d59eb6f79b463f70f60b7568a
Amin <facklak@mail.ru>
2022-08-15 17:13:46 +0300

    fead: finish test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor(minValue,maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.ceil((this.minValue + this.maxValue) / 2);
     }
 
     lower() {
-
+        this.maxValue = Math.ceil((this.minValue + this.maxValue) / 2)
     }
 
     greater() {
-
+        this.minValue = Math.ceil((this.minValue + this.maxValue) / 2)
     }
 }
 
